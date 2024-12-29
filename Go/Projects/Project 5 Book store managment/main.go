47e0e437da68e1d28da75a3e4140d6ae31d57997
MainFoldFunc <gs.live.pub@gmail.com>
2024-12-29 12:09:16 +0100

    Bug fixing

---
+++ Go/Projects/Project 5 Book store managment/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/MainFoldFunc/Learning-other-languages/Go/Projects/Project_5_Book_store_managment/PKG/routes"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r) // Ensure this function is implemented in the routes package
+	http.Handle("/", r)
+
+	log.Println("Starting server on localhost:9010...")
+	if err := http.ListenAndServe("localhost:9010", r); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
+
