8f3ae5e7c0b5a4dc54eeac51bbdab3f1b56b44ff
MainFoldFunc <gs.live.pub@gmail.com>
2024-12-29 10:47:18 +0100

    Progress is made

---
+++ Go/Projects/Project 5 Book store managment/PKG/config/app.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect(){
+	d, err := gorm.Open("mysql", "MainFoldFunc:2010/simplerest?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+	db = d
+}
+func getdb() *gorm.DB {
+	return db
+}
---
+++ Go/Projects/Project 5 Book store managment/PKG/models/book.go
@@ -0,0 +1,7 @@
+package models
+
+import(
+	"github.com/jinzhu/gorm"
+	"github.com/MainFoldFunc/Learning-other-languages/go/Projects/Project 5 Bookstore managment/PKG/config"
+)
+
