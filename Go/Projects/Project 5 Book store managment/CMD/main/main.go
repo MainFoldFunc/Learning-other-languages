7ebbb466cc11439a84d1729cc2d9f7472a3d1d6d
MainFoldFunc <gs.live.pub@gmail.com>
2024-12-29 11:07:29 +0100

    Finished the controlers file

--- Go/Projects/Project 5 Book store managment/CMD/main/main.go
+++ Go/Projects/Project 5 Book store managment/CMD/main/main.go
@@ -9,4 +9,9 @@ import (
 	"github.com/MainFoldFunc/Learning-other-languages/go/Projects/Project 5 Bookstore managment/PKG/routes"
 )
 
-
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/",r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
--- Go/Projects/Project 5 Book store managment/PKG/models/book.go
+++ Go/Projects/Project 5 Book store managment/PKG/models/book.go
@@ -19,3 +19,26 @@ func init() {
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func (b *Book) CreateBook() *Book{
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var books []Book
+	db.Find(&books)
+	return books
+}
+func GetBookByID(ID in64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID = ?", ID).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID = ?", ID).Delete(book)
+	return book
+}
